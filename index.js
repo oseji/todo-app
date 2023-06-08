d0632096f0c25bd9880693fed695d628a4ce138c
oseji <jiade1233@gmail.com>
2023-06-08 16:53:57 +0100

    added mobile responsiveness

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ let completedTodo = [];
 let activeTodo = [];
 
 const bgBody = document.querySelector("body");
-const bgImg = document.querySelector(".bgImg");
+const bgImgDesktop = document.querySelector(".bgImgDesktop");
+const bgImgMobile = document.querySelector(".bgImgMobile");
 const themeIcon = document.querySelector(".themeSwitch");
 const todoMain = document.querySelector(".todoMain");
 const todoStatus = document.querySelector(".todoStatusBar");
+const todoStatusMobile = document.querySelector(".todoStatusMobile");
 const todoInput = document.querySelector(".inputTask");
 const todo = document.querySelectorAll(".todo");
-
 const activeTaskCounter = document.getElementById("numStatus");
 
-console.log(bgBody);
+console.log(todoStatusMobile);
 
 //CODE
 //CHANGING THE THEME
@@ -24,21 +25,27 @@ themeIcon.addEventListener("click", (e) => {
 
   if (text.includes("moon")) {
     themeIcon.src = "images/icon-sun.svg";
-    bgImg.src = "images/bg-desktop-dark.jpg";
+    bgImgDesktop.src = "images/bg-desktop-dark.jpg";
+    bgImgMobile.src = "images/bg-mobile-dark.jpg";
     bgBody.classList.remove("lightThemeBody");
     bgBody.classList.add("darkTheme");
     todoMain.classList.toggle("darkThemeTodo");
     todoStatus.classList.toggle("darkThemeTodo");
+    todoStatusMobile.classList.toggle("darkThemeTodo");
     todoInput.classList.toggle("darkThemeTodo");
   } else if (text.includes("sun")) {
     themeIcon.src = "images/icon-moon.svg";
-    bgImg.src = "images/bg-desktop-light.jpg";
+    bgImgDesktop.src = "images/bg-desktop-light.jpg";
+    bgImgMobile.src = "images/bg-mobile-light.jpg";
     bgBody.classList.remove("darkTheme");
     bgBody.classList.add("lightThemeBody");
     todoMain.classList.toggle("darkThemeTodo");
     todoStatus.classList.toggle("darkThemeTodo");
+    todoStatusMobile.classList.toggle("darkThemeTodo");
     todoInput.classList.toggle("darkThemeTodo");
   }
+
+  console.log(todoStatusMobile.classList);
 });
 
 //ADDING AND DISPLAYING INPUTTED TASKS
